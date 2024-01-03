bf215e348486823ff2875ee0b37b7f0b15d7fc21
SeroTomas <serocontacto@gmail.com>
2024-01-02 22:10:08 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "c-softBlue": "hsl(215, 51%, 70%)",
+        "c-cyan": "hsl(178, 100%, 50%)",
+        "c-blueMain": "hsl(217, 54 %, 11 %)",
+        "c-blueCard": "hsl(216, 50 %, 16 %)",
+        "c-blueLine": "hsl(215, 32 %, 27 %)"
+      }
+    },
+  },
+  plugins: [],
+}
+
